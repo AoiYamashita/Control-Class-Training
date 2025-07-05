edf581d97687681a50c787ad2da58281ad4e73ce
yamashita <yamashitaaoi1230@icloud.com>
2025-07-05 12:38:55 +0900

    add fibo

---
+++ class3/fibo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int fib(int N){
+    if(N == 0)return 0;
+    if(N <= 1)return 1;
+    return fib(N-1)+fib(N-2);
+}
+
+int main(void){
+    printf("%d\n",fib(22));
+    return 0;
+}
